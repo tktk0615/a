46704a65df34bcce89b91e8d7f41ef712157e113
tktk0615 <144981098+tktk0615@users.noreply.github.com>
2023-09-21 04:40:11 +0000

    pppp

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,6 @@ st.set_page_config(page_title="タイトル", layout="wide")
 # タイトルを設定
 st.title("おみくじアプリ")
 if st.button ("おみくじを引く"):
-    results["大吉","中吉","小吉","吉","凶","大凶"]
+    results = ["大吉","中吉","小吉","吉","凶","大凶"]
     result = random.choice(results)
     st.write(f"結果:{result}")
